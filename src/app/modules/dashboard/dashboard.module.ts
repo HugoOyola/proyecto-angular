001207df539d83e2948fba8d55dbaa1f05e06657
Hugo <hugo.ham@outlook.com>
2025-04-25 22:03:23 -0500

    feat: add dashboard and auth modules with navigation menu
    - Integrated DashboardModule into AppModule.
    - Created AuthRoutingModule and AuthModule for authentication features.
    - Implemented NavMenuComponent for navigation within the dashboard.
    - Developed DashboardComponent with a responsive layout using Angular Material.
    - Added routing modules for both dashboard and auth modules.
    - Updated index.html to include Material Icons and Roboto font.
    - Enhanced global styles in styles.scss for better typography and layout.

---
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardComponent } from './dashboard.component';
+
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
+import {MatListModule} from '@angular/material/list';
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    NavMenuComponent
+  ],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule
+  ],
+  exports: [
+    DashboardComponent
+  ],
+})
+export class DashboardModule { }
